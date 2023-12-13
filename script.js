2e34a26247c35c8b28a6b4d96fd247b57b26a1c5
Yonatan Yuryev <yonilight333@gmail.com>
2023-12-13 19:50:16 +0200

    Payment translate

--- script.js
+++ script.js
@@ -1,38 +1,27 @@
 $(document).ready(function () {
-  let seconds = 15;
-  const timeValue = $(".timer__value");
-
-  const timeout = () =>
-    setInterval(() => {
-      if (seconds < 0) {
-        clearInterval(timeout);
-        seconds = 15;
-        return timeout();
-      }
-      timeValue.text(seconds);
-      seconds--;
-    }, 1000);
-
-  // timeout();
-
   const closeIcon = $(".chat-icon-close");
-  closeIcon.hide();
   const messageIcon = $(".chat-icon-message");
 
-  $(".chat-icon").on("click", () => {
-    const chat = $(".chat");
-    const isOpen = chat.attr("data-open");
+  function onClickChat() {
+    $(".chat-icon").on("click", () => {
+      const chat = $(".chat");
+      const isOpen = chat.attr("data-open");
 
-    if (isOpen === "true") {
-      closeIcon.hide();
-      messageIcon.show();
-      chat.attr("data-open", false);
-    } else {
-      closeIcon.show();
-      messageIcon.hide();
-      chat.attr("data-open", true);
-    }
+      if (isOpen === "true") {
+        closeIcon.hide();
+        messageIcon.show();
+        chat.attr("data-open", false);
+      } else {
+        closeIcon.show();
+        messageIcon.hide();
+        chat.attr("data-open", true);
+      }
+    });
+  }
 
-    console.log(isOpen);
-  });
+  function onClickNavItem() {
+    
+  }
+
+  onClickChat();
 });
